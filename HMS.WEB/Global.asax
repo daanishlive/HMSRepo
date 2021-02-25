b9dbc70ffde8a5315874fb0006e38ce60709a272
Daanish Gaur <daanishlive@outlook.com>
2021-02-25 21:30:01 +0530

    AddedWeb LAyer

---
+++ HMS.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HMS.WEB.MvcApplication" Language="C#" %>
---
+++ HMS.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HMS.WEB.Startup))]
+namespace HMS.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
