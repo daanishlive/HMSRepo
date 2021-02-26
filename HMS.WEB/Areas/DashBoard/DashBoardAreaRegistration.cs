f162a8a6d65f0831d0fa74ec0dc841ccdee5b0bb
Daanish Gaur <daanishlive@outlook.com>
2021-02-26 12:26:08 +0530

    Added Template For DashBoard And LogOf Functnality

---
+++ HMS.WEB/Areas/DashBoard/DashBoardAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HMS.WEB.Areas.DashBoard
+{
+    public class DashBoardAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DashBoard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DashBoard_default",
+                "DashBoard/{controller}/{action}/{id}",
+                new {controller="DashBoard", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
