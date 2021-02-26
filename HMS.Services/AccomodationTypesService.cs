8dbd5df69e922b2d228ad823aa51d9d8854acedf
Daanish Gaur <daanishlive@outlook.com>
2021-02-26 21:14:33 +0530

    Curd Operation Related To Accomodation (Service)

--- HMS.Services/AccomodationTypesService.cs
+++ HMS.Services/AccomodationTypesService.cs
@@ -16,10 +16,41 @@ namespace HMS.Services
            return context.AcomodationTypes.ToList();
         }
 
+        public IEnumerable<AcomodationType> SearchAccomodationType(string searchTerm)
+        {
+            var context = new HMSContext();
+            var acomodationTypes= context.AcomodationTypes.AsQueryable();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                acomodationTypes = acomodationTypes.Where(a => a.Name.ToLower().Contains(searchTerm.ToLower())); 
+            }
+            return acomodationTypes.ToList();
+        }
+
+        public AcomodationType GetAccomodationType(int ID)
+        {
+            var context = new HMSContext();
+            return context.AcomodationTypes.Find(ID);
+        }
+
         public bool saveAccomodationType(AcomodationType acomodationType)
         {
             var context = new HMSContext();
             context.AcomodationTypes.Add(acomodationType);
+            return context.SaveChanges() > 0;
+        }
+
+        public bool updateAccomodationType(AcomodationType acomodationType)
+        {
+            var context = new HMSContext();
+            context.Entry(acomodationType).State = System.Data.Entity.EntityState.Modified;
+            return context.SaveChanges() > 0;
+        }
+
+        public bool deleteAccomodationType(AcomodationType acomodationType)
+        {
+            var context = new HMSContext();
+            context.Entry(acomodationType).State = System.Data.Entity.EntityState.Deleted;
             return context.SaveChanges() > 0;
         }
     }
